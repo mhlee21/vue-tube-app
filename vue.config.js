db96f9a8f974ddc44fc827736cf42528a04fcabd
mhlee <mihyun0921@gmail.com>
2022-05-12 21:08:41 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'/vue-tube-app/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-tube-app/docs/'
+    : '/',
   outputDir: './docs/',
 })
